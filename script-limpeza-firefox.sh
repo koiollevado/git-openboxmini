c3274869a1df00f9a37371efba8c1d65686ee179
koiollevado <70024104+koiollevado@users.noreply.github.com>
2021-04-19 11:14:04 -0300

    Add files via upload

--- script-limpeza-firefox.sh
+++ script-limpeza-firefox.sh
@@ -1,8 +1,7 @@
 
 #!/bin/bash
 
-rm /home/$USER/.cache/chromium/Default/Code\ Cache/js/*
-rm /home/$USER/.cache/chromium/Default/Cache/*
+rm /home/$USER/.cache/mozilla/firefox/217x97uu.default-release/cache2/entries/*
 rm /home/$USER/.local/share/Trash/info/*
 rm /home/$USER/.local/share/Trash/files/*
 cd /home/$USER/.local/share/Trash/files/
