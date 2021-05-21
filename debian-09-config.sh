c52b0146f89f9ceb69e539bc022a0337ba2a03d9
koiollevado <70024104+koiollevado@users.noreply.github.com>
2021-05-21 19:49:44 -0300

    Add files via upload

---
+++ debian-09-config.sh
@@ -0,0 +1,62 @@
+
+#!/bin/bash
+
+# configurando o papel de parede aleatorio do desktop
+mv wallpapers ~/.wallpapers
+mkdir ~/Imagens/wallpapers
+chmod +x ~/.wallpapers
+echo "/home/$USER/.wallpapers" >> ~/.config/openbox/autostart
+
+# instala Icons suru 4all e os temas windows 8 e windows 10
+cat xa* > Suru-4All.tar.gz
+mkdir -p ~/.local/share/icons
+tar -vzxf Suru-4All.tar.gz
+mv Suru\ 4All/ ~/.local/share/icons
+unzip Win8.zip
+unzip Win10.zip
+mkdir -p ~/.local/share/themes
+mv Win8/ ~/.local/share/themes
+mv Win10/ ~/.local/share/themes
+
+# configuracoes manuais
+clear
+echo " "
+echo "Permissão para reiniciar e desligar o computador"
+echo "Edite o arquivo /etc/sudoers"
+echo "E adicione ao final do arquivo a seguinte linha"
+echo " "
+echo " "
+echo "$( whoami ) ALL=(ALL) NOPASSWD: /sbin/reboot, /sbin/shutdown, /sbin/poweroff"
+echo " "
+echo "Certifique-se de adicionar ao final uma linha em branco."
+sudo geany /etc/sudoers
+
+# configurando o login automático
+clear
+echo " "
+echo "Configurando o login automático de usuário."
+echo " "
+echo "Deseja configurar o login automático de usuário?"
+echo -n "Digite uma letra (C)onfigura o usuário. (S)air"; read resposta
+case "$resposta" in
+    S|s|"")
+        echo "Saindo da configuração..."
+	sleep 5
+	exit
+    ;;
+    c|C)
+	echo "Edite o arquivo /etc/slim.conf como usuário root."
+	echo 'Digite o comando "leafpad /etc/slim.conf"'
+	echo "Adicione ao final do arquivo as seguintes linhas"
+	echo " "
+	echo " "
+	echo "default_user        $( whoami )"
+	echo "auto_login          yes"
+	echo " "
+	echo "Certifique-se de adicionar ao final uma linha em branco."
+	sudo geany /etc/slim.conf
+    ;;
+    *)
+        echo "Opção inválida"
+    ;;
+esac
