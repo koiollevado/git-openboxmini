40a0244545c30ca58a3f9b47394f23597e15eaa7
koiollevado <70024104+koiollevado@users.noreply.github.com>
2021-05-26 10:38:06 -0300

    Update debian-00-root-sources-driver_video.sh

--- debian-00-root-sources-driver_video.sh
+++ debian-00-root-sources-driver_video.sh
@@ -15,14 +15,6 @@ echo "Atualizando a lista do repositório"
 sudo apt update
 sudo apt upgrade
 
-clear
-
-echo "Instalando os drivers da placa de vídeo."
-lspci | grep -i vga
-echo "Insira o nome do drive vga listado: " ; read driver
-sudo apt search $driver
-echo "Insira o nome do pacote a ser instalado: " ; read pacote
-sudo apt install $pacote
 sudo apt-get install -f
 
-sudo reboot
+clear
