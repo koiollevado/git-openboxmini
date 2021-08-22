2fbf2b8a5317503afebd2f9393c33b2c7a613878
koiollevado <70024104+koiollevado@users.noreply.github.com>
2021-08-22 14:36:39 -0300

    Update debian-0011-root-sources.sh

--- debian-0011-root-sources.sh
+++ debian-0011-root-sources.sh
@@ -1,1 +1,26 @@
 
+#!/bin/bash
+
+clear
+echo " "
+echo "Insira uma senha para o ROOT"
+echo " "
+sudo passwd root
+
+clear
+
+sudo mv /etc/apt/sources.list /etc/apt/sources.list-backup
+sudo mv debian-10-sources.list /etc/apt/sources.list
+
+clear
+
+echo " "
+echo "Atualizando a lista do repositório"
+sudo apt update
+sudo apt upgrade
+
+sudo apt install -f
+
+clear
+
+sudo apt search xserver-xorg-video-
