74788c8cff72cdaed0eca7ef3354961a68313a95
koiollevado <70024104+koiollevado@users.noreply.github.com>
2021-11-19 19:36:52 -0300

    Add files via upload

---
+++ debian-0010-root-sources.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+echo " "
+echo "Insira uma senha para o ROOT"
+echo " "
+sudo passwd root
+
+clear
+
+sudo mv /etc/apt/sources.list /etc/apt/sources.list-backup
+sudo mv debian-10-sources.list /etc/apt/sources.list
+
+clear
+
+echo " "
+echo "Atualizando a lista do repositório"
+sudo apt update
+sudo apt upgrade
+
+sudo apt install -f
+
+clear
+
+sudo apt search xserver-xorg-video-
