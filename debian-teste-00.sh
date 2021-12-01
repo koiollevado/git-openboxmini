7eb25a652a49d0ff671aecde854cced0ac8c5486
koiollevado <70024104+koiollevado@users.noreply.github.com>
2021-12-01 08:55:21 -0300

    Update debian-teste-00.sh

--- debian-teste-00.sh
+++ debian-teste-00.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+clear
 echo " "
 echo "Insira uma senha para o root"
+echo " "
 sudo passwd root
 clear
+sudo mv /etc/apt/sources.list /etc/apt/sources.list-backup
+sudo mv debian-10-sources.list /etc/apt/souces.list
+clear
+echo " "
+echo "Atualizando a lista do repositório"
+echo " "
 sudo apt update
 sudo apt upgrade
 sudo apt -f install
