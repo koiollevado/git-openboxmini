a43b7e36c1c16a92d7aa1be2db3967416c89662e
koiollevado <70024104+koiollevado@users.noreply.github.com>
2021-12-01 12:39:48 -0300

    Update debian-0010-root-sources-openbox.sh

--- debian-0010-root-sources-openbox.sh
+++ debian-0010-root-sources-openbox.sh
@@ -7,13 +7,18 @@ echo " "
 sudo passwd root
 clear
 sudo mv /etc/apt/sources.list /etc/apt/sources.list-backup
-sudo mv debian-10-sources.list /etc/apt/souces.list
+sudo mv debian-10-sources.list /etc/apt/sources.list
 clear
 echo " "
 echo "Atualizando a lista do repositório"
 echo " "
 sudo apt update
 sudo apt upgrade
-sudo apt install openbox obmenu obconf lxappearance lxterminal geany
+sudo apt install openbox -y
+sudo apt install obmenu -y
+sudo apt install obconf -y
+sudo apt install lxappearance -y
+sudo apt install lxterminal -y
+sudo apt install geany -y
 sudo apt -f install
 sudo reboot
