1ae4559e5fe3908b9e752d01cb84738de75d5085
koiollevado <70024104+koiollevado@users.noreply.github.com>
2023-10-26 17:19:39 -0300

    Update debian-9-config.sh

--- debian-9-config.sh
+++ debian-9-config.sh
@@ -8,14 +8,14 @@ chmod +x ~/.wallpapers
 echo "/home/$USER/.wallpapers" >> ~/.config/openbox/autostart
 
 # instala Icons suru 4all e La Captaine e os temas windows 8 e windows 10
-cat xa* > Suru-4All.tar.gz
-mkdir -p ~/.local/share/icons
-tar -vzxf Suru-4All.tar.gz
-mv Suru\ 4All/ ~/.local/share/icons
-mkdir -p ~/.local/share/themes
-unzip Win8.zip -d ~/.local/share/themes
-unzip Win10.zip -d ~/.local/share/themes
-unzip lacapitaine.zip -d ~/.local/share/icons
+# cat xa* > Suru-4All.tar.gz
+# mkdir -p ~/.local/share/icons
+# tar -vzxf Suru-4All.tar.gz
+# mv Suru\ 4All/ ~/.local/share/icons
+# mkdir -p ~/.local/share/themes
+# unzip Win8.zip -d ~/.local/share/themes
+# unzip Win10.zip -d ~/.local/share/themes
+# unzip lacapitaine.zip -d ~/.local/share/icons
 
 # configuracoes manuais
 clear
