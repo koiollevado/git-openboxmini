963ffaaf4479ee66ae4f4b1f3b97d3613d12ea58
koiollevado <70024104+koiollevado@users.noreply.github.com>
2023-11-01 08:59:21 -0300

    Update instalador-64bits.sh

--- printer-epson-l210/instalador-64bits.sh
+++ printer-epson-l210/instalador-64bits.sh
@@ -14,6 +14,8 @@ sudo dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
 
 sudo dpkg -i epson-printer-utility_1.0.2-1lsb3.2_amd64.deb
 
+tar xvzf iscan-bundle-2.30.4.x64.deb.tar.gz
+
 ./iscan-bundle-2.30.4.x64.deb/install.sh
 
 sudo apt -f install
