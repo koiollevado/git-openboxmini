41b4d94936e6fa2d11b9809eef7640d3297f74da
koiollevado <70024104+koiollevado@users.noreply.github.com>
2024-02-21 17:07:14 -0300

    Update instalador-64bits.sh

--- printer-epson-l210/instalador-64bits.sh
+++ printer-epson-l210/instalador-64bits.sh
@@ -10,6 +10,11 @@ sudo apt install lsb-base -y
 
 sudo apt install lsb-release -y
 
+sudo apt install cups -y
+sudo apt install printer-driver-escpr -y
+sudo apt install printer-driver-gutenprint -y
+sudo apt install system-config-printer -y
+
 sudo dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
 
 sudo dpkg -i epson-printer-utility_1.0.2-1lsb3.2_amd64.deb
