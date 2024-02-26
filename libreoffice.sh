7e1d7e93ccb8e849dcce1ae349bab0c19920eaf8
koiollevado <70024104+koiollevado@users.noreply.github.com>
2024-02-26 11:24:36 -0300

    Update libreoffice.sh

--- libreoffice.sh
+++ libreoffice.sh
@@ -1,33 +1,12 @@
 
 #!/bin/bash
 
-echo " "
-echo "Atualizando o repositório... Aguarde..."
-echo " "
 sudo apt update
-echo " "
-echo "Removendo arquivos obsoletos... Aguarde..."
-echo " "
-sudo apt install -f
-echo " "
-clear
-echo " "
-echo "Instalando o libreoffice writer... Aguarde..."
-echo " "
-sudo apt install libreoffice-writer -y
-clear
-echo " "
-echo "Instalando o libreoffice calc... Aguarde..."
-echo " "
-sudo apt install libreoffice-calc -y
-clear
-echo " "
-echo "Instalando o libreoffice impress... Aguarde..."
-echo " "
-sudo apt install libreoffice-impress -y
-clear
-echo " "
-echo "Instalando a tradução libreoffice... Aguarde..."
-echo " "
-sudo apt install libreoffice-l10n-pt-br -y
+
+sudo apt install libreoffice-writer \
+                 libreoffice-calc \
+                 libreoffice-impress \
+                 libreoffice-l10n-pt-br
+
+sudo apt install -f                 
 exit
