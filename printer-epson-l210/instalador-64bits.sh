c9e752291c2f4bde9e144829920aa31624ab906c
koiollevado <70024104+koiollevado@users.noreply.github.com>
2024-02-26 10:31:40 -0300

    Update instalador-64bits.sh

--- printer-epson-l210/instalador-64bits.sh
+++ printer-epson-l210/instalador-64bits.sh
@@ -1,26 +1,21 @@
-#!/bin/bash
-
 sudo apt update
-
 sudo apt -f install
 
-sudo dpkg -i lsb_4.1-48_amd64.deb
+sudo apt install lsb \
+                 lsb-base \
+                 lsb-compact \
+                 lsb-release \
+                 cups \
+                 printer-driver-escpr \
+                 printer-driver-gutenprint \
+                 system-config-printer 
 
-sudo apt install lsb-base -y
-
-sudo apt install lsb-release -y
-
-sudo apt install cups -y
-sudo apt install printer-driver-escpr -y
-sudo apt install printer-driver-gutenprint -y
-sudo apt install system-config-printer -y
-
+# sudo dpkg -i lsb_4.1-48_amd64.deb
 sudo dpkg -i epson-inkjet-printer-201207w_1.0.0-1lsb3.2_amd64.deb
-
-sudo dpkg -i epson-printer-utility_1.0.2-1lsb3.2_amd64.deb
+sudo dpkg -i epson-printer-utility_1.1.2-1_amd64.deb
 
 tar xvzf iscan-bundle-2.30.4.x64.deb.tar.gz
-
+chmod +x /iscan-bundle-2.30.4.x64.deb/install.sh
 ./iscan-bundle-2.30.4.x64.deb/install.sh
 
 sudo apt -f install
