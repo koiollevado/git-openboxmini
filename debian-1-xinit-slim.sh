00e95580fadc11f4a8ccafad7ff971b61be9e906
koiollevado <70024104+koiollevado@users.noreply.github.com>
2024-03-04 12:48:12 -0300

    Update debian-1-xinit-slim.sh

--- debian-1-xinit-slim.sh
+++ debian-1-xinit-slim.sh
@@ -4,8 +4,9 @@ clear
 echo " "
 echo "Instalando o gerenciador gráfico e o login"
 echo " "
-sudo apt install xinit \
-                 slim
+sudo apt install xinit -y
+clear  
+sudo apt install slim -y
 clear                 
 sudo dpkg-reconfigure slim
 clear
