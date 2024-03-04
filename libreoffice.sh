59bee4f0aee132ecb2f6f3427c1e973840857b3f
koiollevado <70024104+koiollevado@users.noreply.github.com>
2024-03-04 13:01:09 -0300

    Update libreoffice.sh

--- libreoffice.sh
+++ libreoffice.sh
@@ -3,10 +3,10 @@
 
 sudo apt update
 
-sudo apt install libreoffice-writer \
-                 libreoffice-calc \
-                 libreoffice-impress \
-                 libreoffice-l10n-pt-br
+sudo apt install libreoffice-writer -y
+sudo apt install libreoffice-calc -y
+sudo apt install office-impress -y
+sudo apt install libreoffice-l10n-pt-br -y
 
 sudo apt install -f                 
 exit
